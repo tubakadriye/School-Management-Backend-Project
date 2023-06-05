e058c7a75f25b8624d79770b84f249942026746f
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-05 16:41:41 +0200

    V2

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -10,10 +10,6 @@ public class SchoolManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
 
-
-        //first line of my code
-        //second line of my code
-
     }
 
 }
