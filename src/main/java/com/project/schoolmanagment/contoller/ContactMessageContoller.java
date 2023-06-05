1ace2621712e6edffd32a61075b325b9109c2b1a
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-05 17:42:47 +0200

    V4

--- src/main/java/com/project/schoolmanagment/contoller/ContactMessageContoller.java
+++ src/main/java/com/project/schoolmanagment/contoller/ContactMessageContoller.java
@@ -1,6 +1,23 @@
 package com.project.schoolmanagment.contoller;
 
+import com.project.schoolmanagment.payload.response.ContactMessageResponse;
+import com.project.schoolmanagment.payload.response.ResponseMessage;
+import com.project.schoolmanagment.service.ContactMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("contactMessages")
+@RequiredArgsConstructor
 public class ContactMessageContoller {
+
+	private final ContactMessageService contactMessageService;
+
+	public ResponseMessage<ContactMessageResponse> save(){
+		return null;
+	}
 
 
 
---
+++ src/main/java/com/project/schoolmanagment/payload/request/ContactMessageRequest.java
@@ -0,0 +1,13 @@
+package com.project.schoolmanagment.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ContactMessageRequest {
+}
