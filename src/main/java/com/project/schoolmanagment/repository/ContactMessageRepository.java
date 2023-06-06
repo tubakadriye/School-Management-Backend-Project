4af12e8635fb48777801494191ab5e6cc059ada2
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-06 15:59:00 +0200

    second day starts

--- src/main/java/com/project/schoolmanagment/repository/ContactMessageRepository.java
+++ src/main/java/com/project/schoolmanagment/repository/ContactMessageRepository.java
@@ -6,8 +6,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 
 public interface ContactMessageRepository extends JpaRepository<ContactMessage,Long> {
-
+	/*
+	 * existsByEmailEqualsAndDateEquals(String email, LocalDate date)
+	 * This method checks if a contact message exists by comparing the email
+	 * and date values with the provided email
+	 * and date parameters. It uses the equals method for comparison.
+	 * existsByEmailAndDate(String email, LocalDate date)
+	 * This method also checks if a contact message exists, but it compares the email
+	 * and date values using the default comparison behavior, which is generally equivalent
+	 * to using the == operator.
+	 * So, the main difference lies in the comparison method used for the email
+	 * and date parameters. The first method (existsByEmailEqualsAndDateEquals) uses
+	 * the equals method, which compares the values of the two objects, while the second method
+	 * (existsByEmailAndDate) uses the default comparison behavior,
+	 * which compares the references of the two objects.
+	 */
 	boolean existsByEmailEqualsAndDateEquals(String email, LocalDate date);
+
+	boolean existsByEmailAndDate(String email, LocalDate date);
+
+
 
 
 }
