807ddb1efb6bfb8f14ef849ebeb53ea88ffa8768
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-06 16:42:19 +0200

    V2

--- src/main/java/com/project/schoolmanagment/utils/Messages.java
+++ src/main/java/com/project/schoolmanagment/utils/Messages.java
@@ -1,4 +1,6 @@
 package com.project.schoolmanagment.utils;
 
 public class Messages {
+	//CRTL+SHIFT+U
+	public static final String ALREADY_SEND_A_MESSAGE_TODAY = "Error: You have already send a message with this e-mail";
 }
