4901a5eecee9f18d52fb9c8802524e36478fa691
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-07 17:45:22 +0200

    day-3 V3

---
+++ src/main/java/com/project/schoolmanagment/entity/abstracts/User.java
@@ -0,0 +1,57 @@
+package com.project.schoolmanagment.entity.abstracts;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.schoolmanagment.entity.concretes.UserRole;
+import com.project.schoolmanagment.entity.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//https://www.baeldung.com/lombok-builder-inheritance
+@SuperBuilder
+public abstract class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String username;
+
+	@Column(unique = true)
+	private String ssn;
+
+	private String name;
+
+	private String surname;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "yyyy-MM-dd")
+	private LocalDate birthDay;
+
+	private String birtPlace;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private String password;
+
+	@Column(unique = true)
+	private String phoneNumber;
+
+	@OneToOne
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private UserRole userRole;
+
+	private Gender gender;
+
+
+
+
+}
