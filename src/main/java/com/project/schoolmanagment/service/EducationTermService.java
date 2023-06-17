97af72c85f055d68dbab3d26e4e056c56c2e1053
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-17 17:43:46 +0200

    day-11 EducationTermService related messages

--- src/main/java/com/project/schoolmanagment/service/EducationTermService.java
+++ src/main/java/com/project/schoolmanagment/service/EducationTermService.java
@@ -1,9 +1,11 @@
 package com.project.schoolmanagment.service;
 
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
 import com.project.schoolmanagment.payload.request.EducationTermRequest;
 import com.project.schoolmanagment.payload.response.EducationTermResponse;
 import com.project.schoolmanagment.payload.response.ResponseMessage;
 import com.project.schoolmanagment.repository.EducationTermRepository;
+import com.project.schoolmanagment.utils.Messages;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,15 @@ public class EducationTermService {
 
 	public ResponseMessage<EducationTermResponse> saveEducationTerm(EducationTermRequest educationTermRequest){
 
+
+
+	}
+
+	private void checkEducationTermDate(EducationTermRequest educationTermRequest){
+
+		if(educationTermRequest.getLastRegistrationDate().isAfter(educationTermRequest.getStartDate())){
+			throw new ResourceNotFoundException(Messages.)
+		}
 	}
 
 
--- src/main/java/com/project/schoolmanagment/utils/Messages.java
+++ src/main/java/com/project/schoolmanagment/utils/Messages.java
@@ -17,4 +17,10 @@ public class Messages {
 	public static final String ALREADY_REGISTER_MESSAGE_EMAIL = "Error: User with email %s is already registered";
 	//CRTL+SHIFT+U
 	public static final String ALREADY_SEND_A_MESSAGE_TODAY = "Error: You have already send a message with this e-mail";
+
+	//education term related messages
+	public static final String EDUCATION_START_DATE_IS_EARLIER_THAN_LAST_REGISTRATION_DATE = "Error: The start date cannot be earlier than the last registration date " ;
+	public static final String EDUCATION_END_DATE_IS_EARLIER_THAN_START_DATE = "Error: The end date cannot be earlier than the start date " ;
+	public static final String EDUCATION_TERM_IS_ALREADY_EXIST_BY_TERM_AND_YEAR_MESSAGE = "Error: Education Term with Term And Year already exist " ;
+	public static final String EDUCATION_TERM_NOT_FOUND_MESSAGE = "Error: Education Term with id %s not found" ;
 }
