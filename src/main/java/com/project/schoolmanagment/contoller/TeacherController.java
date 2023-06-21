4c2e37ff43c0229b0018ce3356091502f909e53d
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-21 23:12:05 +0200

    Teacher delete, find

--- src/main/java/com/project/schoolmanagment/contoller/TeacherController.java
+++ src/main/java/com/project/schoolmanagment/contoller/TeacherController.java
@@ -5,14 +5,13 @@ import com.project.schoolmanagment.payload.response.ResponseMessage;
 import com.project.schoolmanagment.payload.response.TeacherResponse;
 import com.project.schoolmanagment.service.TeacherService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("teachers")
@@ -27,6 +26,38 @@ public class TeacherController {
 		return teacherService.saveTeacher(teacherRequest);
 	}
 
+	@PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+	@GetMapping("/getAll")
+	public List<TeacherResponse> getAllTeacher(){
+		return teacherService.getAllTeacher();
+	}
+
+	@PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+	@GetMapping("/getTeacherByName")
+	public List<TeacherResponse> getTeacherByName(@RequestParam (name = "name") String teacherName){
+		return teacherService.getTeacherByName(teacherName);
+	}
+
+	@PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+	@DeleteMapping("/delete/{id}")
+	public ResponseMessage deleteTeacherById(@PathVariable Long id){
+		return teacherService.deleteTeacherById(id);
+	}
+	@PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+	@GetMapping("/getSavedTeacherById/{id}")
+	public ResponseMessage<TeacherResponse> findTeacherById(@PathVariable Long id){
+		return teacherService.getTeacherById(id);
+	}
+
+	@PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+	@GetMapping("/search")
+	public Page<TeacherResponse> search(
+			@RequestParam(value = "page")int page,
+			@RequestParam(value = "size") int size,
+			@RequestParam(value = "sort") String sort,
+			@RequestParam(value = "type") String type){
+		return teacherService.findTeacherByPage(page,size,sort,type);
+	}
 
 
 
--- src/main/java/com/project/schoolmanagment/service/TeacherService.java
+++ src/main/java/com/project/schoolmanagment/service/TeacherService.java
@@ -4,6 +4,7 @@ import com.project.schoolmanagment.entity.concretes.LessonProgram;
 import com.project.schoolmanagment.entity.concretes.Teacher;
 import com.project.schoolmanagment.entity.enums.RoleType;
 import com.project.schoolmanagment.exception.BadRequestException;
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
 import com.project.schoolmanagment.payload.mappers.TeacherDto;
 import com.project.schoolmanagment.payload.request.TeacherRequest;
 import com.project.schoolmanagment.payload.response.ResponseMessage;
@@ -12,11 +13,15 @@ import com.project.schoolmanagment.repository.TeacherRepository;
 import com.project.schoolmanagment.utils.Messages;
 import com.project.schoolmanagment.utils.ServiceHelpers;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -63,6 +68,52 @@ public class TeacherService {
 				.object(teacherDto.mapTeacherToTeacherResponse(savedTeacher))
 				.build();
 	}
+
+
+	public List<TeacherResponse>getAllTeacher(){
+		return teacherRepository.findAll()
+				.stream()
+				.map(teacherDto::mapTeacherToTeacherResponse)
+				.collect(Collectors.toList());
+	}
+
+	public List<TeacherResponse>getTeacherByName(String teacherName){
+		return teacherRepository.getTeachersByNameContaining(teacherName)
+				.stream()
+				.map(teacherDto::mapTeacherToTeacherResponse)
+				.collect(Collectors.toList());
+	}
+
+	public ResponseMessage deleteTeacherById(Long id){
+		isTeacherExist(id);
+		teacherRepository.deleteById(id);
+
+		return ResponseMessage.builder()
+				.message("Teacher deleted successfully")
+				.httpStatus(HttpStatus.OK)
+				.build();
+	}
+
+
+	private void isTeacherExist(Long id){
+		teacherRepository.findById(id).orElseThrow(()->new ResourceNotFoundException(String.format(Messages.NOT_FOUND_USER_MESSAGE,id)));
+	}
+
+	public ResponseMessage<TeacherResponse>getTeacherById(Long id){
+		isTeacherExist(id);
+
+		return ResponseMessage.<TeacherResponse>builder()
+				.object(teacherDto.mapTeacherToTeacherResponse(teacherRepository.findById(id).get()))
+				.message("Teacher successfully found")
+				.httpStatus(HttpStatus.OK)
+				.build();
+	}
+
+	public Page<TeacherResponse> findTeacherByPage(int page,int size,String sort,String type){
+		Pageable pageable = serviceHelpers.getPageableWithProperties(page, size, sort, type);
+		return teacherRepository.findAll(pageable).map(teacherDto::mapTeacherToTeacherResponse);
+	}
+
 
 
 
