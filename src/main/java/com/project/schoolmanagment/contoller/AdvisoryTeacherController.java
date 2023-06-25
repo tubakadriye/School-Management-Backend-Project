c77ef432c732d640585c0b1f6a025e72a26ec58c
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-25 20:02:53 +0200

    AdvisorTeacherResponse

---
+++ src/main/java/com/project/schoolmanagment/contoller/AdvisoryTeacherController.java
@@ -0,0 +1,15 @@
+package com.project.schoolmanagment.contoller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/advisorTeacher")
+@RestController
+@RequiredArgsConstructor
+public class AdvisoryTeacherController {
+
+	public List<Ad>
+}
