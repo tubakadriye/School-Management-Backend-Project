ce38c2f95c76fd387ea9852eb0811432517a4a92
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-06-26 17:27:21 +0200

    StudentInfoResponse and UpdateStudentInfoRequest

---
+++ src/main/java/com/project/schoolmanagment/contoller/StudentInfoController.java
@@ -0,0 +1,16 @@
+package com.project.schoolmanagment.contoller;
+
+import com.project.schoolmanagment.payload.response.ResponseMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/studentInfo")
+@RequiredArgsConstructor
+public class StudentInfoController {
+
+
+	public ResponseMessage<Stu>
+
+}
