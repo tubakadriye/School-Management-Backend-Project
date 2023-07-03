21a61c1caec45bc926be0f04616e0344db45cb64
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-07-03 20:24:59 +0200

    meet response and request

--- src/main/java/com/project/schoolmanagment/contoller/MeetController.java
+++ src/main/java/com/project/schoolmanagment/contoller/MeetController.java
@@ -1,14 +1,27 @@
 package com.project.schoolmanagment.contoller;
 
+import com.project.schoolmanagment.payload.response.ResponseMessage;
+import com.project.schoolmanagment.service.MeetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("meet")
 @RequiredArgsConstructor
 public class MeetController {
 
+	private final MeetService meetService;
+
+	public ResponseMessage<MeetResponse>saveMeet(HttpServletRequest httpServletRequest,
+	                                             @RequestBody @Valid MeetRequest meetRequest){
+		String username = (String) httpServletRequest.getAttribute("username");
+		return meetService.saveMeet(username,meetRequest);
+	}
 
 
 
